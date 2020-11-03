d733e191a680ad0c4928f5dfb5203b77f5b81c87
Adam Dziomdziora <adam.dziomdziora@cybercom.com>
2020-11-03 21:19:17 +0200

    NO_SQL step1

---
+++ NO_SQL/step1/1.sh
@@ -0,0 +1,18 @@
+docker build . --no-cache -t nosqlwithcheck
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q) 
+docker run --name nosqlwithcheckname -d nosqlwithcheck 
+docker ps | tee log.log
+docker logs nosqlonename | tee -a log.log
+
+grep log.log -e "27017/tcp" | tee test.log
+grep log.log -e "MongoDB starting" | tee -a test.log
+grep log.log -e "\"address\":\"127.0.0.1\"" | tee -a test.log
+
+# and here check
+docker build ./checker --no-cache -t checkerone
+docker run checkerone | tee checkerlog.log
+
+grep checkerlog.log -e "Mongo server is avaiable at (172.17.0.2:27017)" | tee -a test.log
+clear
+cat test.log | wc -l
