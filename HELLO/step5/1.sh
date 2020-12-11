1891b67792b5b93a0c6e5c8cdfe40de4b4618705
Adam Dziomdziora <adam.dziomdziora@dokt.p.lodz.pl>
2020-12-11 11:14:54 +0200

    d11

---
+++ HELLO/step5/1.sh
@@ -0,0 +1,13 @@
+docker build . --no-cache -t copypartone | tee buildlog.log
+grep buildlog.log -e "This file has a long content." | tee test.log
+grep buildlog.log -e "Actually, a really long one." | tee -a test.log
+grep buildlog.log -e "rw.*[\w\W]*Hello_Name_In_Docker_Filesystem\.txt" | tee -a test.log
+clear
+out=$(cat test.log | wc -l)
+if [ "$out" != "3" ]; then
+    echo "Expected '3', but your result is '$out'"
+    exit 1
+else
+    echo "Good job :)"
+    exit 0
+fi
