168c9ee3df88ab21dc0166b320c9c23f84b04373
Adam Dziomdziora <adam.dziomdziora@dokt.p.lodz.pl>
2020-12-11 11:08:00 +0200

    d9

---
+++ HELLO/step4/1.sh
@@ -0,0 +1,17 @@
+docker build . --no-cache -t cmdvsentrypoint
+docker run cmdvsentrypoint | tee log.log
+grep log.log -e "hello (from ENTRYPOINT): world (this part is from CMD" | tee test.log
+docker run cmdvsentrypoint "FINLAND (overridden parameter)" | tee log.log
+grep log.log -e "hello (from ENTRYPOINT): FINLAND (overridden parameter)" | tee test2.log
+clear
+
+out=$(cat test.log | wc -l)
+out2=$(cat test2.log | wc -l)
+out3=$out+$out2
+if [ "$out3" != "1+1" ]; then
+    echo "Expected '1+1', but your result is '$out3'"
+    exit 1
+else
+    echo "Good job :)"
+    exit 0
+fi
