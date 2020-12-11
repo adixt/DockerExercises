4c441cdd993002b70530a94e371d0a9457deda7b
Adam Dziomdziora <adam.dziomdziora@dokt.p.lodz.pl>
2020-12-11 10:48:26 +0200

    d1

--- HELLO/step0/1.sh
+++ HELLO/step0/1.sh
@@ -1,2 +1,11 @@
 docker build . --no-cache | tee log.log
 grep log.log -e greetings | tee test.log
+clear
+out=$(cat test.log | wc -l)
+if [ "$out" != "2" ]; then
+    echo "Expected '1', but your result is '$out'"
+    exit 1
+else
+    echo "Good job :)"
+    exit 0
+fi
